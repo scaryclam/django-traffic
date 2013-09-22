78438926d48dd6520c2fc028242056a474665c08
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2013-09-22 16:08:56 +0100

    Added model

--- trafficlive/www/apps/user/models.py
+++ trafficlive/www/apps/user/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.signals import post_save
+
+
+class TrafficUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        user = self.model(email, password)
+        return user
+
+    def create_superuser(self, email, password=None)
+        user = self.model(email, password)
+        return user
+
+
+class TrafficUser(AbstractBaseUser):
+    objects = TrafficUserManager()
 
 
 class UserProfile(models.Model):
@@ -14,3 +29,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 post_save.connect(create_user_profile, sender='auth.User')
+
